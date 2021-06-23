fd96ec5a835ed5879827070ba0bb9165e7ed3641
JerryHopper <hopper.jerry@gmail.com>
2021-06-23 22:05:12 +0200

    http://163.158.88.17

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://www.handigmannetje.nl');
+  http.get('http://163.158.88.17');
   sleep(1);
 }
 
