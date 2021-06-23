d5da27fe5bc3f1375f10a1a72da3c1d3097e1ea5
JerryHopper <hopper.jerry@gmail.com>
2021-06-23 22:01:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://bidentify.jerryhopper.com');
+  http.get('http://www.handigmannetje.nl');
   sleep(1);
 }
 
