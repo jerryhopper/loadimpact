de92c2140ef849e59e99470c1ccab8fa1cfba6db
JerryHopper <hopper.jerry@gmail.com>
2021-06-24 17:52:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://163.158.88.17:81');
+  http.get('http://163.158.88.17');
   sleep(1);
 }
 
